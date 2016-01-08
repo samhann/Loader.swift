fbd8d89995f91d434978d29e50703ef89db001cd
Samhan <samhan@Arun.local>
2016-01-08 14:57:34 +0530

    Initial commit

---
+++ Loader.swift
@@ -0,0 +1,211 @@
+//
+//  FBLoader.swift
+//  FBAnimatedView
+//
+//  Created by Samhan on 08/01/16.
+//  Copyright © 2016 Samhan. All rights reserved.
+//
+
+import UIKit
+
+
+public extension UITableView
+{
+    public func ld_visibleContentViews()->[UIView]
+    {
+        
+        return (self.visibleCells as NSArray).valueForKey("contentView") as! [UIView]
+        
+    }
+}
+
+public extension UICollectionView
+{
+    public func ld_visibleContentViews()->[UIView]
+    {
+        
+        return (self.visibleCells() as NSArray).valueForKey("contentView") as! [UIView]
+        
+    }
+}
+
+extension UIColor {
+    
+    static func backgroundFadedGrey()->UIColor
+    {
+        return UIColor(red: (246.0/255.0), green: (247.0/255.0), blue: (248.0/255.0), alpha: 1)
+    }
+    
+    static func gradientFirstStop()->UIColor
+    {
+        return  UIColor(red: (238.0/255.0), green: (238.0/255.0), blue: (238.0/255.0), alpha: 1.0)
+    }
+    
+    static func gradientSecondStop()->UIColor
+    {
+        return UIColor(red: (221.0/255.0), green: (221.0/255.0), blue:(221.0/255.0) , alpha: 1.0);
+    }
+}
+
+
+
+public class Loader
+{
+    public static func addLoaderToViews(let views : [UIView])
+    {
+        CATransaction.begin()
+        views.forEach { $0.ld_addLoader() }
+        CATransaction.commit()
+    }
+    
+    public static func removeLoaderFromViews(let views: [UIView])
+    {
+        CATransaction.begin()
+        views.forEach { $0.ld_removeLoader() }
+        CATransaction.commit()
+    }
+
+    public static func addLoaderToTableView(let table : UITableView )
+    {
+        self.addLoaderToViews(table.ld_visibleContentViews())
+    }
+    
+    public static func addLoaderToCollectionView(let coll : UICollectionView )
+    {
+        self.addLoaderToViews(coll.ld_visibleContentViews())
+    }
+
+    public static func removeLoaderFromTableView(let table : UITableView )
+    {
+        self.removeLoaderFromViews(table.ld_visibleContentViews())
+    }
+    
+    public static func removeLoaderFromCollectionView(let coll : UICollectionView )
+    {
+        self.removeLoaderFromViews(coll.ld_visibleContentViews())
+    }
+
+}
+
+class CutoutView : UIView
+{
+    
+    override func drawRect(rect: CGRect) {
+        
+        super.drawRect(rect)
+        
+        let context = UIGraphicsGetCurrentContext()
+        
+        CGContextSetFillColorWithColor(context, UIColor.whiteColor().CGColor)
+        
+        CGContextFillRect(context, self.bounds)
+        
+        for view in (self.superview?.subviews)! {
+            
+            if view != self {
+                
+                CGContextSetBlendMode(context, .Clear);
+                CGContextSetFillColorWithColor(context, UIColor.clearColor().CGColor)
+                CGContextFillRect(context, view.frame)
+            }
+        }
+    }
+}
+
+var cutoutHandle: UInt8 = 0
+var gradientHandle: UInt8 = 0
+
+extension UIView
+{
+    public func ld_getCutoutView()->UIView?
+    {
+        return objc_getAssociatedObject(self, &cutoutHandle) as! UIView?
+    }
+    
+    func ld_setCutoutView(aView : UIView)
+    {
+        return objc_setAssociatedObject(self, &cutoutHandle, aView, .OBJC_ASSOCIATION_RETAIN)
+    }
+    
+    func ld_getGradient()->CAGradientLayer?
+    {
+        return objc_getAssociatedObject(self, &gradientHandle) as! CAGradientLayer?
+    }
+    
+    func ld_setGradient(aLayer : CAGradientLayer)
+    {
+        return objc_setAssociatedObject(self, &gradientHandle, aLayer, .OBJC_ASSOCIATION_RETAIN)
+    }
+    
+    public func ld_addLoader()
+    {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = CGRectMake(0, 0, self.bounds.size.width , self.bounds.size.height)
+        self.layer.insertSublayer(gradient, atIndex:0)
+        
+        self.configureAndAddAnimationToGradient(gradient)
+        self.addCutoutView()
+    }
+    
+    public func ld_removeLoader()
+    {
+        self.ld_getCutoutView()?.removeFromSuperview()
+        self.ld_getGradient()?.removeAllAnimations()
+        self.ld_getGradient()?.removeFromSuperlayer()
+        
+        for view in self.subviews {
+            view.alpha = 1
+        }
+    }
+    
+    
+    func configureAndAddAnimationToGradient(let gradient : CAGradientLayer)
+    {
+        gradient.startPoint = CGPointMake(-1.25, 0)
+        gradient.endPoint = CGPointMake(1.25, 0)
+        
+        gradient.colors = [
+            UIColor.backgroundFadedGrey().CGColor,
+            UIColor.gradientFirstStop().CGColor,
+            UIColor.gradientSecondStop().CGColor,
+            UIColor.gradientFirstStop().CGColor,
+            UIColor.backgroundFadedGrey().CGColor
+        ]
+        
+        gradient.locations = [NSNumber(double: -1.25),NSNumber(double:-1.25),NSNumber(double:0),NSNumber(double: 0.25),NSNumber(double: 1.25)]
+        
+        let gradientAnimation = CABasicAnimation(keyPath: "locations")
+        gradientAnimation.fromValue = [NSNumber(double: 0.0),NSNumber(double:0.0),NSNumber(double:0.10),NSNumber(double: 0.25),NSNumber(double: 1)]
+        
+        gradientAnimation.toValue = [NSNumber(double: 0),NSNumber(double:1),NSNumber(double:1),NSNumber(double: 1.15),NSNumber(double: 1.25)]
+        
+        gradientAnimation.repeatCount = Float.infinity
+        gradientAnimation.fillMode = kCAFillModeForwards
+        gradientAnimation.removedOnCompletion = false
+        
+        gradientAnimation.duration = 1
+        gradient.addAnimation(gradientAnimation ,forKey:"locations")
+        
+        self.ld_setGradient(gradient)
+        
+    }
+    
+    func addCutoutView()
+    {
+        let cutout = CutoutView()
+        cutout.frame = self.bounds
+        cutout.backgroundColor = UIColor.clearColor()
+        
+        self.insertSubview(cutout, atIndex: 1)
+        cutout.setNeedsDisplay()
+        
+        
+        for view in self.subviews {
+            if view != cutout {
+                view.alpha = 0
+            }
+        }
+        self.ld_setCutoutView(cutout)
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,24 @@
+## Loader.Swift 
+
+This library allows you to easily add an FB style animated loading placeholder to your tableviews or collection views.
+
+![Preview](http://g.recordit.co/xAV7KP5lCz.gif)
+
+## Usage
+
+```swift
+
+Loader.addLoaderToTableView(self.tableView) 		// to add 
+Loader.removeLoaderFromTableView(self.tableView)	// to remove
+
+```
+
+## How it works
+
+It adds a gradient to the content views of the visible cells. After that it inserts a cutout view wherever all the other views are with "holes" where all the text and image views are. The alphas of all the text and image views are set to zero.
+
+This is undone when you remove the loader.
+
+## Credits
+
+http://cloudcannon.com/deconstructions/2014/11/15/facebook-content-placeholder-deconstruction.html
